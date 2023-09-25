aeba3d8b041bb871dc38e560535e7f7aae88ab50
Alex <tindall.al@gmail.com>
2023-09-25 12:44:37 -0500

    Updated some variable namesFinished the write to file method and now it takes the last number of the csv file and increments up by 1 from the previous numberAdded most of the write functionalityadded comment

---
+++ Services/FileManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ApplicationTemplate.Services;
+
+public class FileManager : IFileManager
+{
+    public void ReadFile(string path)
+    {
+        StreamReader sr = new StreamReader(path);
+        int linesToRead = 10;
+        sr.ReadLine();
+
+
+
+        while (!sr.EndOfStream)
+    {
+            Console.WriteLine("Press Enter to read the next 10 lines or press Escape to exit.");
+            Console.WriteLine();
+        for (int i = 0; i < linesToRead; i++)
+        {
+            var line = sr.ReadLine();
+            if (line == null)
+            {
+                break;
+            }
+            Console.WriteLine($"{line}");
+        }
+
+        ConsoleKeyInfo keyInfo = Console.ReadKey();
+        if (keyInfo.Key == ConsoleKey.Escape)
+        {
+            break; 
+        }
+        else if (keyInfo.Key == ConsoleKey.Enter)
+        {
+            Console.WriteLine(); 
+        }
+    }
+
+    sr.Close();
+    }
+
+    public void WriteFile(string path)
+    {
+        int genreCount;
+        bool isValidInput = false;
+        string[] genreArray = new string[100];
+
+        var movieID = GetLastMovieId(path) + 1; 
+
+        Console.WriteLine("Enter Title");
+        var title = Console.ReadLine();
+
+        while (true)
+        {
+            do
+            {
+                Console.Write("How many Genres is this movie? ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out genreCount))
+                {
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                }
+            } while (!isValidInput);
+
+            for (int i = 0; i < genreCount; i++)
+            {
+                Console.WriteLine($"Genre {i + 1}:");
+                genreArray[i] = Console.ReadLine();
+            }
+            break;
+        }
+
+        string genreString = string.Join("|", genreArray.Take(genreCount));
+
+        using (StreamWriter sw = new StreamWriter(path, true))
+        {
+            sw.WriteLine($"{movieID},{title},{genreString}");
+        }
+    }
+
+    public int GetLastMovieId(string path)
+    {
+        if (!File.Exists(path))
+        {
+            return 0; 
+        }
+
+        var lines = File.ReadLines(path);
+        var lastLine = lines.LastOrDefault(); 
+        if (lastLine != null)
+        {
+            var parts = lastLine.Split(',');
+            if (parts.Length > 0 && int.TryParse(parts[0], out int lastMovieId))
+            {
+                return lastMovieId;
+            }
+        }
+
+        return 0; 
+    }
+}
--- Services/MainService.cs
+++ Services/MainService.cs
@@ -1,60 +1,7 @@
 ﻿using System;
-using System.IO;
 using System.Reflection.PortableExecutable;
 
 namespace ApplicationTemplate.Services;
-
-/// <summary>
-///     You would need to inject your interfaces here to execute the methods in Invoke()
-///     See the commented out code as an example
-/// </summary>
-
-public interface IFileManager
-{
-    void ReadFile(string path);
-    void WriteFile(string path);
-}
-
-public class FileManager : IFileManager
-{
-    public void ReadFile(string path)
-    {
-         StreamReader sr = new StreamReader(path);
-    int linesToRead = 10;
-
-    Console.WriteLine("Press Enter to read the next 10 lines or press Escape to exit.");
-
-    while (!sr.EndOfStream)
-    {
-        for (int i = 0; i < linesToRead; i++)
-        {
-            var line = sr.ReadLine();
-            if (line == null)
-            {
-                break;
-            }
-            Console.WriteLine($"{line}");
-        }
-
-        ConsoleKeyInfo keyInfo = Console.ReadKey();
-        if (keyInfo.Key == ConsoleKey.Escape)
-        {
-            break; // Exit the loop if Escape is pressed.
-        }
-        else if (keyInfo.Key == ConsoleKey.Enter)
-        {
-            Console.WriteLine(); // Move to the next line when Enter is pressed.
-        }
-    }
-
-    sr.Close();
-    }
-
-    public void WriteFile(string path)
-    {
-        throw new NotImplementedException();
-    }
-}
 
 public class MainService : IMainService
 {
@@ -66,6 +13,7 @@ public class MainService : IMainService
     public void Invoke()
     {
         IFileManager _fileManager = new FileManager();
+        string filePath = "movies.csv";
 
         Console.WriteLine("Choose an option:");
         Console.WriteLine("1. Read from File");
@@ -75,7 +23,11 @@ public class MainService : IMainService
 
         if (choice == "1")
         {
-            _fileManager.ReadFile("movies.csv");
+            _fileManager.ReadFile(filePath);
+        }
+        else if (choice == "2")
+        {
+            _fileManager.WriteFile(filePath);
         }
 
     }
