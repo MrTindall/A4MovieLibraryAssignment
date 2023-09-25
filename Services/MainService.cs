0c1501724d2bc28336df2ca05071f24dbfbe5abb
Alex <tindall.al@gmail.com>
2023-09-25 12:14:09 -0500

    Add project files.

---
+++ Services/MainService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ApplicationTemplate.Services;
+
+/// <summary>
+///     You would need to inject your interfaces here to execute the methods in Invoke()
+///     See the commented out code as an example
+/// </summary>
+public class MainService : IMainService
+{
+
+    public MainService()
+    {
+
+    }
+    public void Invoke()
+    {
+        Console.WriteLine("Use this as the main in a sense, put code here so it will be called by the main");
+        Console.ReadLine();
+       
+    }
+}
